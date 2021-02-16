5302346a3a5af9cadccdc571368f4933738d982f
oatswap-dev <oatswap@protonmail.com>
2021-02-16 16:34:50 +0800

    Add test for burn function of OatToken

--- test/CakeToken.test.js
+++ test/CakeToken.test.js
@@ -11,5 +11,16 @@ contract('OatToken', ([alice, bob, carol, dev, minter]) => {
     it('mint', async () => {
         await this.oat.mint(alice, 1000, { from: minter });
         assert.equal((await this.oat.balanceOf(alice)).toString(), '1000');
-    })
+    });
+
+    it('burn', async () => {
+        await this.oat.mint(alice, 1000, { from: minter });
+        await this.oat.mint(bob, 1000, { from: minter });
+        assert.equal((await this.oat.totalSupply()).toString(), '2000');
+        assert.equal((await this.oat.totalBurned()).toString(), '0');
+        await this.oat.burn(200, { from: alice });
+        assert.equal((await this.oat.balanceOf(alice)).toString(), '800');
+        assert.equal((await this.oat.totalSupply()).toString(), '1800');
+        assert.equal((await this.oat.totalBurned()).toString(), '200');
+    });
 });
